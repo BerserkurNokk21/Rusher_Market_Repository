04e72a10eb2526e40bd4e0296d43bb45fd6a36f0
AlphaAnd0mega <148582283+MarcosFargueta@users.noreply.github.com>
2024-11-07 18:57:35 +0100

    Interacciones Jugador

--- Assets/Scripts/Player/Character_Controller.cs
+++ Assets/Scripts/Player/Character_Controller.cs
@@ -4,11 +4,18 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class Character_Controller : NetworkBehaviour
 {
     [SerializeField] PlayerInputs _playerInputs;
     public float moveSpeed;
     public Rigidbody2D rb;
+
+    [SerializeField] private bool stunned;
+    [SerializeField] private float stunTime;
+    [SerializeField] private Collider2D attackCol;
+    [SerializeField] private Transform attackPos;
+    [SerializeField] private float attackRadius;
 
     [SerializeField] private Item_Product nearbyItem;
     [SerializeField] private bool isNearItem = false;
@@ -18,6 +25,7 @@ public class Character_Controller : NetworkBehaviour
     public Vector3 itemCarryOffset = new Vector3(0.5f, 0.5f, 0); // Offset del ítem respecto al jugador
 
     Vector2 moveDir;
+
 
     Enemy enemy;
 
@@ -34,15 +42,21 @@ public class Character_Controller : NetworkBehaviour
 
     void Update()
     {
+        
+        if(!IsOwner){
+            return;
+        }
+        Hit();
         Move();
         if (heldItem != null)
         {
             heldItem.transform.position = transform.position + itemCarryOffset;
         }
-
-        if(!IsOwner){
-            return;
+        if (stunned)
+        {
+            StartCoroutine("Stun");
         }
+
     }
 
     void Move()
@@ -50,7 +64,31 @@ public class Character_Controller : NetworkBehaviour
         moveDir = _playerInputs.PlayerActions.Mover.ReadValue<Vector2>();
         rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
     }
+    private void Hit()
+    {
+        if (_playerInputs.PlayerActions.Attack.triggered && heldItem==null)
+        {
+            Debug.Log("Attack");
+            Collider2D[] enemigos = Physics2D.OverlapCircleAll(attackPos.position, attackRadius, LayerMask.GetMask("Enemigos"));
 
+            foreach(Collider2D enemigo in enemigos) {
+                Character_Controller player = enemigo.GetComponent<Character_Controller>();
+
+                if(player.NetworkObjectId != this.NetworkObjectId)
+                {
+                    HitEnemy(player.gameObject,stunned);
+                }
+            }
+        }
+    }
+
+    IEnumerator Stun()
+    {
+        attackCol.enabled = false;
+        yield return new WaitForSeconds(stunTime);
+        attackCol.enabled = true;
+        StopCoroutine("Stun");
+    }
     void PickUpItem()
     {
         if (isNearItem && nearbyItem != null && heldItem == null)
@@ -67,9 +105,7 @@ public class Character_Controller : NetworkBehaviour
             isNearItem = true;
         }
 
-        if(collision.CompareTag("Enemy")&& _playerInputs.PlayerActions.Attack.triggered){
-            enemy.stunned = true;
-        }
+        
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
@@ -83,5 +119,15 @@ public class Character_Controller : NetworkBehaviour
         {
             enemy.stunned = false;
         }
+    }
+
+    private void HitEnemy(GameObject target, bool stunned)
+    {
+        target.GetComponent<Character_Controller>().stunned = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(attackPos.position, attackRadius);
     }
 }
